1822bec841619199934de2e78b82f9375bb04230
ab2010 <ab116893@gmail.com>
2017-10-20 14:46:45 -0400

    Initial hours, day, location data

--- data/getOperatingHours.py
+++ data/getOperatingHours.py
@@ -9,3 +9,102 @@ Created on Mon Oct  9 16:39:11 2017
 from bs4 import BeautifulSoup
 import urllib.request, urllib.error, urllib.parse
 import time
+
+def make_soup(url):
+        link = urllib.request.urlopen(url)
+        soupdata = BeautifulSoup(link, "html.parser")
+        return soupdata
+soup = make_soup("https://www.unh.edu/dining/regular-hours-operation")
+
+
+   
+for record in soup.find_all("tr"):
+    #list_of_hours=[]
+    for hour in record.find_all(["th","td"]):
+        day = record.text
+        hour_list = "\n" + hour.text
+        dining = day + hour_list
+        
+    print(dining)
+
+
+ #       list_of_hours.append(getdata)
+ #   mylist.append(list_of_hours)
+#print(mylist, "\n")
+
+        
+        
+    
+ 
+            
+        
+ 
+    
+            
+                
+                
+            
+            
+            
+            
+            
+           
+            
+       
+      
+       
+       
+       
+       
+        #hoursdata =hoursdata+","+data.text
+        
+        #hoursdatasave = hoursdatasave + "\n" + hoursdata[1:]
+
+    
+
+    
+        
+    
+    
+
+
+
+
+    
+
+
+    
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+    
+    
+
+
+    
+
+
+    
+
+
+
+
+
