fe85594c0bcccff9ed3a60c239a2c0131fcc7f0f
Huy Le <huyle.finance@gmail.com>
2017-10-19 21:59:43 -0400

    adding getOperatingHours.py file to scrape unh dining halls hours

---
+++ data/getOperatingHours.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 16:39:11 2017
+
+@author: Huy
+"""
+
+from bs4 import BeautifulSoup
+import urllib.request, urllib.error, urllib.parse
+import time
