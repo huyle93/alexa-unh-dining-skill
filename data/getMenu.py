a16b22b37e28fc1eb45b3bb736ff52b7fa3a9f6e
Huy Le <huyle.finance@gmail.com>
2017-11-18 17:08:52 -0500

    checkOpen bug fixed, added 3 json menu and updated getMenu.py

--- data/getMenu.py
+++ data/getMenu.py
@@ -15,48 +15,70 @@ Created on Mon Oct  9 16:39:11 2017
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
+import datetime
+import json
+import pprint
 
 
 
-url_list = ['http://foodpro.unh.edu/shortmenu.asp?sName=University%20Of%20New%20Hampshire%20Hospitality%20Services&locationNum=80&locationName=Holloway%20Dining%20Hall&naFlag=1',
-            'http://foodpro.unh.edu/shortmenu.asp?sName=University%20Of%20New%20Hampshire%20Hospitality%20Services&locationNum=10&locationName=Stillings%20Dining%20Hall&naFlag=1',
-            'http://foodpro.unh.edu/shortmenu.asp?sName=University%20Of%20New%20Hampshire%20Hospitality%20Services&locationNum=30&locationName=Philbrook%20Dining%20Hall&naFlag=1']
+#url_list = ['http://foodpro.unh.edu/shortmenu.asp?sName=University%20Of%20New%20Hampshire%20Hospitality%20Services&locationNum=80&locationName=Holloway%20Dining%20Hall&naFlag=1',
+#            'http://foodpro.unh.edu/shortmenu.asp?sName=University%20Of%20New%20Hampshire%20Hospitality%20Services&locationNum=10&locationName=Stillings%20Dining%20Hall&naFlag=1',
+#            'http://foodpro.unh.edu/shortmenu.asp?sName=University%20Of%20New%20Hampshire%20Hospitality%20Services&locationNum=30&locationName=Philbrook%20Dining%20Hall&naFlag=1']
 
-for url in url_list:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    table = soup.find("td")
-    list = []
-    for date in table.find_all("span", {"class":"shortmenutitledate"}):
-        list.append(date)
-        for menu in table.find_all("div", {"class":"shortmenuheader"}):
-            list.append(menu)
-            for recipes in table.find_all("div", {"class":"shortmenurecipes"}):
+# =================== Retrieval Source =========================
+dining_hall = { "Philbrook":"30", "Stillings":"10", "Holloway":"80"}
 
-                
-                
-#---------------------------------------------------------------------------
-                
-#To test print 
-#                list.append(recipes) 
-               # print(list)
+now = datetime.datetime.now()
+current_day = str(now.day)
+current_month = str(now.month)
+current_year = str(now.year)
+
+url = 'http://foodpro.unh.edu/shortmenu.asp?sName=University+Of+New+Hampshire+Hospitality+Services&locationNum='+dining_hall["Stillings"]+'&locationName=Holloway+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate='+current_month+'%2F'+current_day+'%2F'+current_year
+
+# =================== Retrieval Data =========================
+
+#for url in url_list:
+dictionary = {}
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+table = soup.find("td")
+title_list = []
+menu_list = []
+recipes_list = []
+for date in table.find_all("span", {"class":"shortmenutitledate"}):
+    title_list.append(date.get_text())
+    #print(date.text)
+    for menu in table.find_all("div", {"class":"shortmenuheader"}):
+        menu_list.append(menu.get_text())
+        #print(menu.text)
+        for recipes in table.find_all("div", {"class":"shortmenurecipes"}):
+            recipes_list.append(recipes.get_text())
+            #print(recipes.text)
+            
+         
+dictionary = {"Date" : title_list, "Location" : menu_list, "Food" : recipes_list}
+print(dictionary)
+#pprint.pprint(dictionary, width=1)
 
 
-               
-                
+
             
 
+            
+            
+            
+            
+#                for i in list:
+#                    text = i.get_text()
+#                    print(text)                             
+
+
+
+# =================== Dump JSON =========================
+with open('Stilling.json', 'w') as f:
+     json.dump(dictionary, f)
     
-        
-#    for recipes in record:
-#        recipes.find_all("div", {"class":"shortmenurecipes"})
-#        
-#    
 
-            
-            
-            
-    
     
     
  
